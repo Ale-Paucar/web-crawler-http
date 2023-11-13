7bc317414c0ffff107f8850b01700235e69b9865
Ale <nuala.ni.ale@gmail.com>
2023-11-13 10:47:45 -0500

    getURLSsFromHTML function and test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,34 @@
+const {JSDOM} = require('jsdom');
+
+
+const getURLsfromHTML = (htmlBody, baseUrl) => {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        if(linkElement.href.slice(0,1) === "/"){
+            // * relative
+            try {
+                const  urlObj = new URL(`${baseUrl}${linkElement.href}`);
+                urls.push(urlObj.href);
+                
+            } catch (err) {
+                console.log(`Error with relative url: ${err.message}`)
+            }
+        }else{
+            // * relative
+            try {
+                const  urlObj = new URL(linkElement.href);
+                urls.push(linkElement.href);
+            } catch (err) {
+                console.log(`Error with relative url: ${err.message}`)
+            }
+        }
+    }
+    return urls;
+}
+
+
 const normalizeURL=(urlString)=>{
     const urlObj = new URL(urlString)
     const hostPath  =  `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +39,6 @@ const normalizeURL=(urlString)=>{
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsfromHTML } = require("./crawl.js");
 const {test, expect } = require('@jest/globals');
 
 test("normalizeURL strip protocol", () =>{
@@ -31,4 +31,72 @@ test("normalizeURL strip http", () =>{
     const expectedOut = "mi.pagina.com/algo/algomas";
     expect(actualOutput).toEqual(expectedOut);
 })
+///
+test("getURLsFromHTML", () =>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="http://mi.pagina.com/path/">
+                mi pagina blog.com bro
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseURL = "http://mi.pagina.com/path/";
+    const actualOutput = getURLsfromHTML(inputHTMLBody,inputBaseURL);
+    const expectedOut = ["http://mi.pagina.com/path/"];
+    expect(actualOutput).toEqual(expectedOut);
+})
 
+
+test("getURLsFromHTML relative", () =>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                mi pagina blog.com bro
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseURL = "http://mi.pagina.com";
+    const actualOutput = getURLsfromHTML(inputHTMLBody,inputBaseURL);
+    const expectedOut = ["http://mi.pagina.com/path/"];
+    expect(actualOutput).toEqual(expectedOut);
+})
+
+
+test("getURLsFromHTML both", () =>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="http://mi.pagina.com/path1/">
+                mi pagina blog.com bro path one
+            </a>
+            <a href="/path2/">
+                mi pagina blog.com bro path two XD
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseURL = "http://mi.pagina.com";
+    const actualOutput = getURLsfromHTML(inputHTMLBody,inputBaseURL);
+    const expectedOut = ["http://mi.pagina.com/path1/","http://mi.pagina.com/path2/"];
+    expect(actualOutput).toEqual(expectedOut);
+})
+// !
+test("getURLsFromHTML invalid", () =>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="invalid">
+                invalid URL
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseURL = "http://mi.pagina.com";
+    const actualOutput = getURLsfromHTML(inputHTMLBody,inputBaseURL);
+    const expectedOut = [];
+    expect(actualOutput).toEqual(expectedOut);
+})
