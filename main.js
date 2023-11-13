4725ea44ffa77632b079fcef2751ac4d06213638
Ale <nuala.ni.ale@gmail.com>
2023-11-13 15:29:10 -0500

    crawl single page with page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const { crawlPage } = require('./crawl')
+
+const main = () => {
+    if(process.argv.length < 3){
+        console.log("No website provied");
+        process.exit(1);
+    }
+
+    if(process.argv.length > 3){
+        console.log("to many command line args");
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];    
+    
+    console.log(`starting crawl of ${baseURL}`);
+    crawlPage(baseURL)
+}
+main()
